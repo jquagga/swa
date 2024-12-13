80b7b7cb871be67f55fa0acc07be0e5f6b84da7e
Justin Settle <jus10@partlycloudy.org>
2024-12-12 22:35:20 -0500

    v2

--- src/swa.js
+++ src/swa.js
@@ -1,57 +1,52 @@
-async function main(latitude, longitude, headertxt) {
+// Chart.js support libraries
+import Chart from "chart.js/auto";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import "chartjs-adapter-luxon";
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+async function main(latitude, longitude) {
   const headers = {
     accept: "application/ld+json",
     "user-agent": "https://github.com/jquagga/swa",
   };
 
-  const response = await fetch(
+  const point_response = await fetch(
     `https://api.weather.gov/points/${latitude},${longitude}`,
-    { headers: headers },
+    { headers },
   );
-  if (!response.ok) {
-    const errorContainer =
-      document.getElementById("error-container") ||
-      document.createElement("div");
-    errorContainer.id = "error-container";
-    errorContainer.className = "alert alert-danger";
-    errorContainer.textContent = `Error: ${response.status} ${response.statusText}`;
-    document.body.appendChild(errorContainer);
-    throw new Error("Exiting");
-  }
+  const point = await point_response.json();
 
-  const point = await response.json();
-
-  const alerts = await (
-    await fetch(
-      `https://api.weather.gov/alerts?active=true&status=actual&message_type=alert,update&point=${latitude},${longitude}&limit=50`,
-      { headers: headers },
-    )
-  ).json();
-
-  const forecast = await (
-    await fetch(point.forecast, { headers: headers })
-  ).json();
-
-  const forecastHourly = await (
-    await fetch(point.forecastHourly, { headers: headers })
-  ).json();
-
-  document.getElementById("header").innerHTML = await build_header(
-    headertxt,
-    point,
+  const alerts_response = await fetch(
+    `https://api.weather.gov/alerts?active=true&status=actual&message_type=alert,update&point=${latitude},${longitude}&limit=50`,
+    { headers },
   );
-  document.getElementById("alerts").innerHTML = await alert_processing(alerts);
-  document.getElementById("grid").innerHTML = await build_grid(forecast);
+  const alerts = await alerts_response.json();
+
+  const forecast_response = await fetch(point.forecast, { headers });
+  const forecast = await forecast_response.json();
+
+  const forecastHourly_response = await fetch(point.forecastHourly, {
+    headers,
+  });
+  const forecastHourly = await forecastHourly_response.json();
+
+  document.querySelector("#header").innerHTML = await build_header(point);
+  document.querySelector("#alerts").innerHTML = await alert_processing(alerts);
+  document.querySelector("#grid").innerHTML = await build_grid(forecast);
   await build_chart(forecastHourly);
   await build_map(latitude, longitude);
-  document.getElementById("footer").innerHTML =
-    `<div class="text-center"><a href=https://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude} class="link-primary"><button type="button" class="btn btn-primary">Weather.gov forecast</button></a></div>
-    <p class="text-center">This forecast is generated from the U.S. National Weather Service's <a href="https://www.weather.gov/documentation/services-web-api">weather.gov API</a>
+  document.querySelector("#footer").innerHTML =
+    `<div class="text-center"><a href=https://forecast.weather.gov/MapClick.php?lat=${latitude}&lon=${longitude}
+        class="link-primary"><button type="button" class="btn btn-primary">Weather.gov forecast</button></a></div>
+<p class="text-center">This forecast is generated from the U.S. National Weather Service's <a
+        href="https://www.weather.gov/documentation/services-web-api">weather.gov API</a>
     using this <a href="https://github.com/jquagga/swa">Simple Weather App</a>.</p>`;
 }
 
-async function build_header(headertxt, point) {
-  headertxt += `
+async function build_header(point) {
+  const headertxt = `
     <div class="container">
       <h1>
         Weather for ${point.relativeLocation.city},
@@ -86,6 +81,7 @@ async function alert_processing(alerts) {
         </div>
         `;
   }
+
   return alert_string;
 }
 
@@ -93,18 +89,18 @@ async function build_chart(forecastHourly) {
   for (let i = 0; i < 8; i++) {
     // We have to chop "mph" off of the windspeed to make it just a number
     const windspeed = forecastHourly.periods[i].windSpeed.split(" ");
-    forecastHourly.periods[i].windSpeed = parseFloat(windspeed[0]);
+    forecastHourly.periods[i].windSpeed = Number.parseFloat(windspeed[0]);
     // Now we send the period off to apptemp to create apptemp!
     forecastHourly.periods[i].appTemp = apptempF(
-      parseFloat(forecastHourly.periods[i].temperature),
-      parseFloat(forecastHourly.periods[i].relativeHumidity.value),
+      Number.parseFloat(forecastHourly.periods[i].temperature),
+      Number.parseFloat(forecastHourly.periods[i].relativeHumidity.value),
       forecastHourly.periods[i].windSpeed,
     );
   }
 
   Chart.register(ChartDataLabels);
-  const ctx = document.getElementById("myChart");
-  new Chart(ctx, {
+  const context = document.querySelector("#myChart");
+  new Chart(context, {
     type: "line",
     data: {
       labels: [
@@ -200,7 +196,7 @@ async function build_chart(forecastHourly) {
       },
       plugins: {
         datalabels: {
-          backgroundColor: function (context) {
+          backgroundColor(context) {
             return context.dataset.backgroundColor;
           },
           borderRadius: 25,
@@ -227,37 +223,39 @@ async function build_chart(forecastHourly) {
 }
 
 async function build_grid(forecast) {
-  return `<table class="table table-striped">
-      <tbody>
+  return `
+ <table class="table table-striped">
+    <tbody>
         <tr>
-          <td><b>${forecast.periods[0].name}</b></td>
-          <td>${forecast.periods[0].detailedForecast}</td>
+            <td><b>${forecast.periods[0].name}</b></td>
+            <td>${forecast.periods[0].detailedForecast}</td>
         </tr>
-          <td><b>${forecast.periods[1].name}</b></td>
-          <td>${forecast.periods[1].detailedForecast}</td>
-        </tr>
-        <tr>
-          <td><b>${forecast.periods[2].name}</b></td>
-          <td>${forecast.periods[2].detailedForecast}</td>
+        <td><b>${forecast.periods[1].name}</b></td>
+        <td>${forecast.periods[1].detailedForecast}</td>
         </tr>
         <tr>
-          <td><b>${forecast.periods[3].name}</b></td>
-          <td>${forecast.periods[3].detailedForecast}</td>
+            <td><b>${forecast.periods[2].name}</b></td>
+            <td>${forecast.periods[2].detailedForecast}</td>
         </tr>
         <tr>
-          <td><b>${forecast.periods[4].name}</b></td>
-          <td>${forecast.periods[4].detailedForecast}</td>
+            <td><b>${forecast.periods[3].name}</b></td>
+            <td>${forecast.periods[3].detailedForecast}</td>
         </tr>
         <tr>
-          <td><b>${forecast.periods[5].name}</b></td>
-          <td>${forecast.periods[5].detailedForecast}</td>
+            <td><b>${forecast.periods[4].name}</b></td>
+            <td>${forecast.periods[4].detailedForecast}</td>
         </tr>
-      </tbody>
-    </table>`;
+        <tr>
+            <td><b>${forecast.periods[5].name}</b></td>
+            <td>${forecast.periods[5].detailedForecast}</td>
+        </tr>
+    </tbody>
+</table>
+    `;
 }
 
 async function build_map(latitude, longitude) {
-  let map = L.map("map").setView([latitude, longitude], 8);
+  const map = L.map("map").setView([latitude, longitude], 8);
 
   L.tileLayer(
     "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
@@ -289,54 +287,54 @@ async function build_map(latitude, longitude) {
   what the weather.gov site displays.
 */
 function apptempF(T_F, rh, ws_mph) {
-  // T_F, rh, ws_mph
-  if (T_F <= 51 && ws_mph >= 3.0) {
-    return (
-      35.74 + 0.6215 * T_F + (-35.75 + 0.4275 * T_F) * Math.pow(ws_mph, 0.16)
-    );
+  // Taken right from NWS CAVE and converted to JS:
+  // cave/com.raytheon.viz.gfe/localization/gfe/userPython/smartTools/ApparentTemperature.py
+  // prettier-ignore
+  if (T_F <= 51) {
+    const mag = ws_mph * 1.15;
+    const WindChillValue = mag <= 3 ? T_F : 35.74 + (0.6215 * T_F) - (35.75 * (mag ** 0.16)) + (0.4275 * T_F * (mag ** 0.16));
+    return WindChillValue;
   } else if (T_F > 79) {
-    let hi_F =
-      -42.379 +
-      2.04901523 * T_F +
-      10.14333127 * rh -
-      0.22475541 * T_F * rh -
-      6.83783e-3 * Math.pow(T_F, 2) -
-      5.481717e-2 * Math.pow(rh, 2) +
-      1.22874e-3 * Math.pow(T_F, 2) * rh +
-      8.5282e-4 * T_F * Math.pow(rh, 2) -
-      1.99e-6 * Math.pow(T_F, 2) * Math.pow(rh, 2);
-
-    // Apply an adjustment for low humidities
-    if (rh < 13 && T_F > 80 && T_F < 112) {
-      let adjustment =
-        ((13 - rh) / 4.0) * Math.sqrt((17 - Math.abs(T_F - 95.0)) / 17.0);
-      hi_F -= adjustment;
-      // Apply an adjustment for high humidities
-    } else if (rh > 85 && T_F >= 80 && T_F < 87) {
-      let adjustment = ((rh - 85) / 10.0) * ((87 - T_F) / 5.0);
-      hi_F += adjustment;
-    }
-    return hi_F;
+    const A = -42.379;
+    const B = 2.04901523 * T_F;
+    const C = 10.14333127 * rh;
+    const D = -0.22475541 * T_F * rh;
+    const E = -0.00683783 * (T_F ** 2);
+    const F = -0.05481717 * (rh ** 2);
+    const G = 0.00122874 * (T_F ** 2) * rh;
+    const H = 0.00085282 * T_F * (rh ** 2);
+    const I = -0.00000199 * (T_F ** 2) * (rh ** 2);
+    
+    let HeatIndexValue = A + B + C + D + E + F + G + H + I;
+    
+    // make the adjustments for low humidity
+    const rhLessThan13 = rh < 13.0;
+    const T80to112 = T_F >= 80 && T_F <= 112;
+    const downMask = rhLessThan13 && T80to112;
+    
+    // make array that is T_F where conditions are true and 100, otherwise
+    const adjustT = downMask ? T_F : 80.0;
+    HeatIndexValue[downMask] = HeatIndexValue[downMask] - (((13.0 - rh[downMask]) / 4.0) * Math.sqrt((17.0 - Math.abs(adjustT[downMask] - 95.0)) / 17));
+    
+    // make the adjustments for high humidity
+    const rhGreater85 = rh > 85.0;
+    const T80to87 = T_F >= 80.0 && T_F <= 87.0;
+    const upMask = rhGreater85 && T80to87;
+    HeatIndexValue[upMask] = HeatIndexValue[upMask] + (((rh[upMask] - 85.0) / 10.0) * ((87 - T_F[upMask]) / 5.0));
+    
+    return HeatIndexValue;
   }
 }
 
 const options = { enableHighAccuracy: true, timeout: 15000, maximumAge: 3600 };
 
 async function success(pos) {
-  await main(
-    pos.coords.latitude.toFixed(4),
-    pos.coords.longitude.toFixed(4),
-    "", // This is the pre-header below for an error; not needed for a non-error.
-  );
+  await main(pos.coords.latitude.toFixed(4), pos.coords.longitude.toFixed(4));
 }
 
-async function error(err) {
-  let headertxt = `<div class="alert alert-primary" role="alert">
-            <b>Hello!</b>
-            This site utilizes the U.S. National Weather Service's <a href="https://www.weather.gov/documentation/services-web-api">weather.gov</a> API to generate a local forecast from your geolocated position.
-             You may have answered "no" to the request for location, or your browser wasn't able to find your location so we're going to show the
-             weather in Honolulu, HI.  For more information, you can <a href="https://github.com/jquagga/swa">see the code for this project.</a></div>`;
-  await main(21.3069, -157.8583, headertxt);
+// eslint-disable-next-line no-unused-vars
+async function error(error) {
+  throw new Error("Exiting");
 }
 
-window.navigator.geolocation.getCurrentPosition(success, error, options);
+globalThis.navigator.geolocation.getCurrentPosition(success, error, options);
