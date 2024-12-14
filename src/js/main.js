15d46512ba37f5b5ca5f402d0602c9a3a4101db5
Justin Settle <jus10@partlycloudy.org>
2024-12-14 18:15:00 -0500

    Various cleanup; xo linting; manifest linting

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
-// Import our custom CSS
-import "../scss/styles.scss";
-
+/* eslint-disable import/no-unassigned-import */
+/* eslint-disable quotes */
 // Import all of Bootstrap's JS
-//import * as bootstrap from "bootstrap";
-
+// import * as bootstrap from "bootstrap";
 import "bootstrap/js/dist/alert";
 import "bootstrap/js/dist/button";
-// import 'bootstrap/js/dist/carousel';
+// Import 'bootstrap/js/dist/carousel';
 import "bootstrap/js/dist/collapse";
-// import 'bootstrap/js/dist/dropdown';
+// Import 'bootstrap/js/dist/dropdown';
 import "bootstrap/js/dist/modal";
-// import 'bootstrap/js/dist/offcanvas';
+// Import 'bootstrap/js/dist/offcanvas';
 // import 'bootstrap/js/dist/popover';
 // import 'bootstrap/js/dist/scrollspy';
 // import 'bootstrap/js/dist/tab';
 // import 'bootstrap/js/dist/toast';
 // import 'bootstrap/js/dist/tooltip';
+// Import our custom CSS
+import "../scss/styles.scss";
