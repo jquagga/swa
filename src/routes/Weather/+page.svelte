1cd89da4fc0f4afdbfcb378a9f9764507adeab93
Justin <jquagga@noreply.codeberg.org>
2026-06-01 20:40:28 +0200

    Change User Agent to codeberg (#19)
    Co-authored-by: Justin Settle <jus10@partlycloudy.org>
    Reviewed-on: https://codeberg.org/jquagga/swa/pulls/19

--- src/routes/Weather/+page.svelte
+++ src/routes/Weather/+page.svelte
@@ -75,7 +75,7 @@
 
   const MAX_RETRIES = 3;
   const GRAPH_HOURS = 25;
-  const USER_AGENT = "https://github.com/jquagga/swa";
+  const USER_AGENT = "https://codeberg.org/jquagga/swa";
 
   const DATASET_CONFIG = {
     TEMPERATURE: {
